98afd0338282b28fe3eb9f04be08267cd0b5bffe
Kevinna01 <97471980+Kevinna01@users.noreply.github.com>
2024-12-14 14:28:53 +0100

    Update WarpGod_localization.lua

--- scripts/mods/WarpGod/WarpGod_localization.lua
+++ scripts/mods/WarpGod/WarpGod_localization.lua
@@ -37,25 +37,31 @@ local loc = {
         en = "Enable Warp Unbound Bug Fix",
     },
     warp_unbound_bug_fix_enable_description = {
-        en = "Warp Unbound last 11.5s, sometime around 10.5s there is a probability to explode with Smite, Surge & Trauma staffs. Bugfix disables attacks that generate peril during a interval.",
+        en = "Warp Unbound last 11.5s, at sometime around 10.5s there is a probability to explode with Smite, Surge & Trauma staffs. Bugfix disables attacks that generate peril during a interval.",
     },
     interval1_duration = {
-        en = "Interval-duration",
+        en = "Start-interval duration",
     },
     interval1_duration_description = {
         en = "For the bugfix interval. Default is 0.7",
     },
     interval1_start_delay = {
-        en = "Interval start-delay",
+        en = "Start-interval offset",
     },
     interval1_start_delay_description = {
-        en = "For the Bugfix interval. Default is 0.9",
+        en = "For the Bugfix interval interval to offset interval to start later. Default is 0.9",
     },
     interval2_duration = {
-        en = "Interval-duration",
+        en = "End-interval duration",
     },
     interval2_duration_description = {
         en = "For the end of Warp Unbound buff. Default is 0.5",
+    },
+    interval2_end_delay = {
+        en = "End-interval offset",
+    },
+    interval2_end_delay_description = {
+        en = "For the end of Warp Unbound buff to offset interval to end later. Default is 0.2",
     },
     --[[
     --Additional Settings
